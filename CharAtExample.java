4a9878ae947c4fe81d0e2fa4d21226f3185f3ec0
mathsphile <ndas20997@gmail.com>
2024-07-24 22:44:10 +0530

    Create CharAtExample.java

---
+++ CharAtExample.java
@@ -0,0 +1,7 @@
+public class CharAtExample {
+    public static void main(String args[]) {
+        String name = "javatpoint";
+        char ch = name.charAt(4);// returns the char value at the 4th index
+        System.out.println(ch);
+    }
+}
